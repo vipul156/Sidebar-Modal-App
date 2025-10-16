b259d43539726a5eba9af70a91fd62f559367231
vipul156 <38092536+vipul156@users.noreply.github.com>
2025-10-16 20:53:54 +0530

    Add files via upload

---
+++ src/Sidebar.jsx
@@ -0,0 +1,43 @@
+import { social, links } from './data';
+import { FaTimes } from 'react-icons/fa';
+
+const Sidebar = () => {
+  
+
+  return (
+    <aside
+      className={"sidebar"}
+  >
+      <div className="sidebar-header">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
---
+++ src/context.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useContext } from 'react';
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+  };
+  const closeSidebar = () => {
+  };
+  const openModal = () => {
+  };
+  const closeModal = () => {
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        openSidebar,
+        closeModal,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
