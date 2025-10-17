1784d0e1fab9689f23cf4f9b0144cec278c32338
vipul156 <38092536+vipul156@users.noreply.github.com>
2025-10-17 20:24:51 +0530

    Remove allowedHosts configuration from server

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: [
-      "dchlwg-5173.csb.app", // ✅ Allow your sandbox host
-    ],
-  },
 });
